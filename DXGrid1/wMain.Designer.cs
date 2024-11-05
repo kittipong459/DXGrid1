a9177fd7c493928d1a188d54f6a5435384a18564
kittipong459 <gonfairy459@gmail.com>
2024-11-05 16:48:53 +0700

    Add project files.

---
+++ DXGrid1/wMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DXGrid1
+{
+    partial class wMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ogdData = new DevExpress.XtraGrid.GridControl();
+            this.ogdDataList = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.ockColCheckBox = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.otbColText = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemCheckEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogdData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogdDataList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ogdData
+            // 
+            this.ogdData.Location = new System.Drawing.Point(70, 100);
+            this.ogdData.MainView = this.ogdDataList;
+            this.ogdData.Name = "ogdData";
+            this.ogdData.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemCheckEdit1});
+            this.ogdData.Size = new System.Drawing.Size(814, 444);
+            this.ogdData.TabIndex = 0;
+            this.ogdData.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.ogdDataList});
+            // 
+            // ogdDataList
+            // 
+            this.ogdDataList.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.ockColCheckBox,
+            this.otbColText});
+            this.ogdDataList.GridControl = this.ogdData;
+            this.ogdDataList.Name = "ogdDataList";
+            // 
+            // ockColCheckBox
+            // 
+            this.ockColCheckBox.Caption = "เลือก";
+            this.ockColCheckBox.ColumnEdit = this.repositoryItemCheckEdit1;
+            this.ockColCheckBox.Name = "ockColCheckBox";
+            this.ockColCheckBox.Visible = true;
+            this.ockColCheckBox.VisibleIndex = 0;
+            // 
+            // otbColText
+            // 
+            this.otbColText.Caption = "ข้อมูล";
+            this.otbColText.FieldName = "tDataText";
+            this.otbColText.Name = "otbColText";
+            this.otbColText.Visible = true;
+            this.otbColText.VisibleIndex = 1;
+            // 
+            // repositoryItemCheckEdit1
+            // 
+            this.repositoryItemCheckEdit1.AutoHeight = false;
+            this.repositoryItemCheckEdit1.Name = "repositoryItemCheckEdit1";
+            // 
+            // wMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 570);
+            this.Controls.Add(this.ogdData);
+            this.Name = "wMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ogdData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogdDataList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl ogdData;
+        private DevExpress.XtraGrid.Views.Grid.GridView ogdDataList;
+        private DevExpress.XtraGrid.Columns.GridColumn ockColCheckBox;
+        private DevExpress.XtraGrid.Columns.GridColumn otbColText;
+        private DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit repositoryItemCheckEdit1;
+    }
+}
+
---
+++ DXGrid1/wMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DXGrid1
+{
+    public partial class wMain : DevExpress.XtraEditors.XtraForm
+    {
+        public wMain()
+        {
+            InitializeComponent();
+            
+            try
+            {
+                ogdData.DataSource = W_SEToSetDataExample();
+            }
+            catch (Exception ex) {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        private DataTable W_SEToSetDataExample()
+        {
+            DataTable odtData = new DataTable();
+
+            try
+            {
+                odtData = new DataTable();
+                odtData.Columns.Add("tDataText", typeof(string));
+                odtData.Columns.Add("dDataDateTime", typeof(DateTime));
+                odtData.Columns.Add("nDataNumber", typeof(int));
+                odtData.Columns.Add("cDataDecimal", typeof(decimal));
+
+                odtData.Rows.Add("ข้อความตัวอย่างที่ 1", DateTime.Now, 2000, 1500.50);
+                odtData.Rows.Add("ข้อความตัวอย่างที่ 2", DateTime.Now, 150450, 1000.26);
+                odtData.Rows.Add("ข้อความตัวอย่างที่ 3", DateTime.Now, 1850, 1850);
+                odtData.Rows.Add("ข้อความตัวอย่างที่ 4", DateTime.Now, 999000, 880.7858);
+                odtData.Rows.Add("ข้อความตัวอย่างที่ 5", DateTime.Now, 10, 15.669);
+
+            }
+            catch (Exception)
+            {
+                odtData = null;
+            }
+            finally
+            {
+
+            }
+
+            return odtData;
+        }
+
+
+    }
+}
